9579a45b9fbde01618fa19a76cabfefc7b89827b
johnkoerner <github@riddlepop.com>
2014-06-19 20:58:17 -0400

    Adding missing file

---
+++ PowerShellProvider/MyPowerShellProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation.Provider;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerShellProvider
+{
+    [CmdletProvider("MyPowerShellProvider", ProviderCapabilities.None)]
+    public class MyPowerShellProvider : NavigationCmdletProvider
+    {
+
+        protected override bool IsValidPath(string path)
+        {
+            return true;
+        }
+    }
+}
